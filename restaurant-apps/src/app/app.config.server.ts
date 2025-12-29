a64699afbded2fda61a7057b56390a1c63e382b9
Luiz Morais <Cluiz4003@outlook.pt>
2025-12-29 12:25:48 +0000

    new start

--- restaurant-apps/src/app/app.config.server.ts
+++ restaurant-apps/src/app/app.config.server.ts
@@ -4,8 +4,13 @@ import { appConfig } from './app.config';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    provideServerContext('ssr')
   ]
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
+function provideServerContext(arg0: string): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
+  throw new Error('Function not implemented.');
+}
+
--- restaurant-apps/src/app/app.config.ts
+++ restaurant-apps/src/app/app.config.ts
@@ -1,9 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideServerRendering } from '@angular/platform-server';
+
+
+const serverConfig: ApplicationConfig = {
+  providers: [
+    provideServerRendering(),
+    provideServerContext('ssr')
+  ]
+};
+
+export const config = mergeApplicationConfig( serverConfig);
+function provideServerContext(arg0: string): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
+  throw new Error('Function not implemented.');
+}
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
